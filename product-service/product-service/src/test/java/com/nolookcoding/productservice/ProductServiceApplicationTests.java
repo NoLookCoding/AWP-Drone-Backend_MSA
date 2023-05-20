c38e77b70ee94bf8fdbff2c1541f24e052163c03
SeungGun <saverrw2864@naver.com>
2023-05-20 23:50:23 +0900

    chore: add product service module

---
+++ product-service/product-service/src/test/java/com/nolookcoding/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nolookcoding.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
