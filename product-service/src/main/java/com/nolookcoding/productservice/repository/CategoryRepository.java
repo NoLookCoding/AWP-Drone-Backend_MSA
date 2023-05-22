51d781ff550623c56a5bb323f635bd9a87383319
SeungGun <saverrw2864@naver.com>
2023-05-22 23:48:15 +0900

    fix: Category 엔티티 타입에서 Enum 타입으로 강등

--- product-service/src/main/java/com/nolookcoding/productservice/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nolookcoding.productservice.repository;
-
-import com.nolookcoding.productservice.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
