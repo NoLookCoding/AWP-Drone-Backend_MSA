861849cd4c53dbca0919dddd4290200d5662eee3
SeungGun <saverrw2864@naver.com>
2023-05-23 21:26:26 +0900

    feat: BaseEntity에 대한 Audit 리스너 추가

--- product-service/src/main/java/com/nolookcoding/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/nolookcoding/productservice/ProductServiceApplication.java
@@ -3,9 +3,11 @@ package com.nolookcoding.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaAuditing // Auditing 기능 활성화
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
