aea183bd94790db80714ef1f51573ef65b43db4d
SeungGun <saverrw2864@naver.com>
2023-05-27 01:35:48 +0900

    refactor: 주문 상태 필드 값 업데이트

--- order-service/src/main/java/com/nolookcoding/orderservice/domain/OrderState.java
+++ order-service/src/main/java/com/nolookcoding/orderservice/domain/OrderState.java
@@ -2,7 +2,16 @@ package com.nolookcoding.orderservice.domain;
 
 public enum OrderState {
     PAID,
-    DELIVERY,
+    READY,
+    DELIVERING,
     RECEIVED,
     CANCEL;
+
+    public static OrderState findCategoryByString(String orderState) {
+        for (OrderState o : OrderState.values()) {
+            if (o.name().equalsIgnoreCase(orderState))
+                return o;
+        }
+        return null;
+    }
 }
