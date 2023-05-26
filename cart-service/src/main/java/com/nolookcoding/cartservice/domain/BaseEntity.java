01dfeb9bb68a26b20b815a72bf3b0673771ff438
SeungGun <saverrw2864@naver.com>
2023-05-26 23:56:45 +0900

    refactor: 장바구니 엔티티 수정

---
+++ cart-service/src/main/java/com/nolookcoding/cartservice/domain/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.nolookcoding.cartservice.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    private String dataState = null;
+
+    public void updateDataState(String state) {
+        this.dataState = state;
+    }
+}
