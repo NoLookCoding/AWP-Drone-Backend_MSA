f042ad552f0961dea99b5031492776a093ac8381
SeungGun <saverrw2864@naver.com>
2023-05-27 21:43:26 +0900

    chore: 유저 서비스 CORS 정책 추가

--- user-service/src/main/java/com/nolookcoding/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/nolookcoding/userservice/UserServiceApplication.java
@@ -3,9 +3,11 @@ package com.nolookcoding.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@CrossOrigin("*")
 public class UserServiceApplication {
 
     public static void main(String[] args) {
