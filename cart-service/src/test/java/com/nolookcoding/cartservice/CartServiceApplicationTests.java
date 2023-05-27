a94a86d5f2fdaafbe773f01c3a89f389e012c924
SeungGun <saverrw2864@naver.com>
2023-05-27 14:28:31 +0900

    test: 장바구니 서비스 기본 테스트 코드 삭제

--- cart-service/src/test/java/com/nolookcoding/cartservice/CartServiceApplicationTests.java
+++ cart-service/src/test/java/com/nolookcoding/cartservice/CartServiceApplicationTests.java
@@ -3,11 +3,11 @@ package com.nolookcoding.cartservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class CartServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class CartServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
