4798f66e027cd64242e1f9c28cd31707b64ba6c1
SeungGun <saverrw2864@naver.com>
2023-05-27 14:18:27 +0900

    refactor: 모든 서비스의 기본 Spring Boot 테스트 코드 제거

--- eureka-server/src/test/java/com/nolookcoding/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/nolookcoding/eurekaserver/EurekaServerApplicationTests.java
@@ -3,11 +3,11 @@ package com.nolookcoding.eurekaserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EurekaServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class EurekaServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
--- user-service/src/test/java/com/nolookcoding/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/nolookcoding/userservice/UserServiceApplicationTests.java
@@ -3,11 +3,11 @@ package com.nolookcoding.userservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class UserServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class UserServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
