e12a452fe205c450ae78e2a17bfa49600ea0e5f4
SeungGun <saverrw2864@naver.com>
2023-05-28 20:37:53 +0900

    chore: 불필요한 CrossOrigin 어노테이션 제거

--- gateway/src/main/java/com/nolookcoding/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/nolookcoding/gateway/GatewayApplication.java
@@ -3,9 +3,7 @@ package com.nolookcoding.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("*")
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
